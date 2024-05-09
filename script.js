9fdb1b049994e890fb4d91f3c94facab221f29c2
Meratte <mbartnicka@proton.me>
2024-05-09 17:33:48 -0400

    pushing to git

---
+++ script.js
@@ -0,0 +1,14 @@
+alert('Welcome GoPassion!')
+let age = 13;
+const birthYear = 2011;
+
+console.log("Minimum Age to use the page", age)
+console.log('you can use this page if you were born before this year:', birthYear)
+
+if (age >= 13){
+    console.log('Eligible to use this website.');
+ } else {
+  console.log('Uneligible to use this website - come back later!')
+}
+
+parseInt(document.getElementById("output"))
