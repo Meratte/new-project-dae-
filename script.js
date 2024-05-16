a5fbcd9ab63417f440b4f92ce46d23f49d11f5e0
Meratte <mbartnicka@proton.me>
2024-05-16 16:13:06 -0400

    fixed script

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// script.js
 function findPassion() {
   const interests = document.getElementById('interests').value.split(',').map(item => item.trim());
   const activities = document.getElementById('activities').value.split(',').map(item => item.trim());
   const skills = document.getElementById('skills').value.split(',').map(item => item.trim());
-
 
   const allInputs = [...interests, ...activities, ...skills];
   const uniqueInputs = Array.from(new Set(allInputs));
@@ -15,4 +13,17 @@ function findPassion() {
   } else {
       resultElement.innerText = 'Please enter some interests, activities, or skills to get recommendations.';
   }
+}
+
+let findPassion_age = 17;
+if (findPassion_age < 18) {
+  console.log("You're too young!");
+}
+
+let age = 18;
+let findpassion_test = 19;
+if (findpassion_test === 19 && findPassion_age === 18) {
+  console.log("You can be in a theater");
+} else {
+  console.log(age + findpassion_test);
 }
